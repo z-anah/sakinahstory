8ae3d3aa2cc7ec74afdc95f5d95891bf854b6dcc
z-anah <40556052+z-anah@users.noreply.github.com>
2025-03-28 21:59:48 +0700

    Update Home.jsx

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -4,23 +4,23 @@ const BASE_PATH = import.meta.env.BASE_URL;
 
 const Home = () => {
   return (
-    <div className="background">
-      <img src={`${BASE_PATH}/public/images/rings.png`} alt="My Image" width="120" style={{ marginBottom: '1em' }} />
-      <img src={`${BASE_PATH}/public/images/alhamdulillah.png`} alt="My Image" width="300" style={{ marginBottom: '4em' }} />
-      <p style={{ marginBottom: '4em' }}>BY THE GRACE OF ALLAH (SWT), WE, 3</p>
+    <div className="min-h-screen flex justify-center items-center flex-col text-center bg-[url('/images/mobile.png')] bg-cover bg-center bg-no-repeat">
+      <img src={`${BASE_PATH}/public/images/rings.png`} alt="My Image" width="120" className="mb-4" />
+      <img src={`${BASE_PATH}/public/images/alhamdulillah.png`} alt="My Image" width="300" className="mb-16" />
+      <p className="mb-16">BY THE GRACE OF ALLAH (SWT), WE, 3</p>
       <h1>PUTRI</h1>
       <h2>and</h2>
       <h1>FAHMI</h1>
-      <div style={{ marginBottom: '4em' }} />
+      <div className="mb-16" />
       <p>ARE MARRIED</p>
-      <div style={{ marginBottom: '2em' }} />
+      <div className="mb-8" />
       <p>ON</p>
-      <div style={{ marginBottom: '2em' }} />
+      <div className="mb-8" />
       <h3>11TH APRIL 2025</h3>
-      <div style={{ marginBottom: '2em' }} />
+      <div className="mb-8" />
       <p>FRIDAY, AT 8 O'CLOCK IN THE MORNING</p>
       <p>IN DKI JAKARTA, INDONESIA, PLANET EARTH</p>
-      <div style={{ marginBottom: '2em' }} />
+      <div className="mb-8" />
       <p>AND WE MADE YOU BY PAIR</p>
     </div>
   );
