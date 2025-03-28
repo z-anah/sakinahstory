7c12c967e59b87b40fab033d3be8795d94903d13
z-anah <40556052+z-anah@users.noreply.github.com>
2025-03-28 21:48:27 +0700

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './screens/Home';
-import Project from './screens/Project';
-import Statistics from './screens/Statistics';
-import Transaction from './screens/Transaction';
-import Login from './screens/Login';
-import Signup from './screens/Signup';
-import Category from './screens/Category';
-import NameAccounts from './screens/NameAccounts';
+// import Login from './screens/Login';
 
 const App = () => {
   const BASE_PATH = import.meta.env.BASE_URL;
@@ -15,13 +9,7 @@ const App = () => {
   return (
     <Routes>
       <Route path={BASE_PATH} element={<Home />} />
-      <Route path={`${BASE_PATH}/auth/login`} element={<Login />} />
-      <Route path={`${BASE_PATH}/auth/signup`} element={<Signup />} />
-      <Route path={`${BASE_PATH}/project/:projectId`} element={<Project />} />
-      <Route path={`${BASE_PATH}/project/:projectId/transaction/:transactionId`} element={<Transaction />} />
-      <Route path={`${BASE_PATH}/project/:projectId/statistics`} element={<Statistics />} />
-      <Route path={`${BASE_PATH}/categories`} element={<Category />} />
-      <Route path={`${BASE_PATH}/accounts`} element={<NameAccounts />} />
+      {/* <Route path={`${BASE_PATH}/auth/login`} element={<Login />} /> */}
       <Route path="*" element={<Navigate to={BASE_PATH} />} />
     </Routes>
   );
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,148 +1,28 @@
-import { collection, addDoc, getDocs } from 'firebase/firestore';
-import { db } from 'src/firebase/config';
-import { useEffect, useState } from 'react';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { Link } from 'react-router-dom';
-import LoadingSpinner from '../components/LoadingSpinner';
-import ErrorMessage from '../components/ErrorMessage';
+import '../styles/Home.css';
 
 const BASE_PATH = import.meta.env.BASE_URL;
+console.log(`BASE_PATH: ${BASE_PATH}`);
 
 const Home = () => {
-  const [isFetching, setIsFetching] = useState(true);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [projects, setProjects] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const auth = getAuth();
-    const token = localStorage.getItem('authToken');
-    if (!token) {
-      setIsAuthenticated(false);
-      setIsFetching(false);
-      return;
-    }
-
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setIsAuthenticated(true);
-        getProjects();
-      } else {
-        setIsAuthenticated(false);
-        setIsFetching(false);
-      }
-    });
-
-    return () => unsubscribe();
-  }, []);
-
-  async function getProjects() {
-    try {
-      const projects = [];
-      const querySnapshot = await getDocs(collection(db, 'projects'));
-      querySnapshot.forEach((doc) => {
-        projects.push({ id: doc.id, ...doc.data() });
-      });
-      const sortedProjects = projects.sort((a, b) => b.createdAt - a.createdAt);
-      setProjects(sortedProjects);
-    } catch (error) {
-      console.error(error);
-      setError('Failed to fetch projects');
-    } finally {
-      setIsFetching(false);
-    }
-  }
-
-  async function handleAddProject(e) {
-    setIsFetching(true);
-    e.preventDefault();
-    const name = e.target.name.value;
-    const description = e.target.description.value;
-    try {
-      await addDoc(collection(db, 'projects'), {
-        name,
-        description,
-        createdAt: new Date(),
-        userId: localStorage.getItem('userId'),
-      });
-      await getProjects(); 
-    } catch (error) {
-      console.error(error);
-      setError('Failed to add project');
-    } finally {
-      setIsFetching(false);
-    }
-  }
-
-  if (isFetching) {
-    return <LoadingSpinner />;
-  }
-
-  if (!isAuthenticated) {
-    return <ErrorMessage message='Please login to view this page' />;
-  }
-
-  const menuItems = [
-    { href: `${BASE_PATH}/categories`, label: 'Categories' },
-    { href: `${BASE_PATH}/accounts`, label: 'Accounts' },
-  ];
-
   return (
-    <div className='flex items-center justify-center bg-gray-100'>
-      <div className='w-full max-w-sm p-4'>
-        <h1 className='mb-4 text-2xl font-bold'>Home</h1>
-        <form className='space-y-4' onSubmit={handleAddProject}>
-          <div>
-            <label htmlFor='Name'>Name</label>
-            <input
-              type='text'
-              id='name'
-              name='name'
-              className='w-full rounded border border-gray-300 p-2'
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor='Description'>Description</label>
-            <input
-              type='text'
-              id='description'
-              name='description'
-              className='w-full rounded border border-gray-300 p-2'
-              required
-            />
-          </div>
-          <button type='submit' className='my-2 w-full rounded bg-blue-500 p-2 text-white'>
-            Add Project
-          </button>
-        </form>
-        {error && <ErrorMessage message={error} />}
-
-        <h2 className='mt-4 text-xl font-bold'>Projects</h2>
-        <ul className='my-2 rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white'>
-          {projects.map((project) => (
-            <Link 
-              to={`${BASE_PATH}/project/${project.id}`} 
-              key={project.id}
-              relative="path"
-            >
-              <li className='w-full border-b border-gray-200 px-4 py-2 dark:border-gray-600'>
-                {project.name}
-              </li>
-            </Link>
-          ))}
-        </ul>
-        <h2 className='mt-4 text-xl font-bold'>Menu</h2>
-        <ul className='my-2 rounded-lg border border-gray-200 bg-white text-sm font-medium text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white'>
-          {menuItems.map((item, index) => (
-            <Link to={item.href} key={index}>
-              <li className='w-full border-b border-gray-200 px-4 py-2 dark:border-gray-600'>
-                {item.label}
-              </li>
-            </Link>
-          ))}
-        </ul>
-      </div>
+    <div className="background">
+      <img src={`${BASE_PATH}/public/images/rings.png`} alt="My Image" width="120" style={{ marginBottom: '1em' }} />
+      <img src={`${BASE_PATH}/public/images/alhamdulillah.png`} alt="My Image" width="300" style={{ marginBottom: '4em' }} />
+      <p style={{ marginBottom: '4em' }}>BY THE GRACE OF ALLAH (SWT), WE, 3</p>
+      <h1>PUTRI</h1>
+      <h2>and</h2>
+      <h1>FAHMI</h1>
+      <div style={{ marginBottom: '4em' }} />
+      <p>ARE MARRIED</p>
+      <div style={{ marginBottom: '2em' }} />
+      <p>ON</p>
+      <div style={{ marginBottom: '2em' }} />
+      <h3>11TH APRIL 2025</h3>
+      <div style={{ marginBottom: '2em' }} />
+      <p>FRIDAY, AT 8 O'CLOCK IN THE MORNING</p>
+      <p>IN DKI JAKARTA, INDONESIA, PLANET EARTH</p>
+      <div style={{ marginBottom: '2em' }} />
+      <p>AND WE MADE YOU BY PAIR</p>
     </div>
   );
 };
