aa24dd6c3979bce5d770a52b83283e45f53ca8f1
z-anah <40556052+z-anah@users.noreply.github.com>
2025-03-28 17:59:41 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/sakinahstory/', // Must match the GitHub repo name
+    baseURL: '/sakinahstory.github.io/',
   },
 
   routeRules: {
