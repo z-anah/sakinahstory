e9fb426bbf2f2510ce11cf16150699dac0830183
z-anah <40556052+z-anah@users.noreply.github.com>
2025-03-28 17:52:19 +0700

    ok

--- app.vue
+++ app.vue
@@ -1,5 +1,85 @@
 <template>
-  <div>
-    Hello
+  <div class="background">
+
+    <img src="/images/rings.png" alt="My Image" width="120" style="margin-bottom: 1em;">
+
+    <img src="/images/alhamdulillah.png" alt="My Image" width="300" style="margin-bottom: 4em;">
+
+    <p style="margin-bottom: 4em;">BY THE GRACE OF ALLAH (SWT), WE,</p>
+
+    <h1>PUTRI</h1>
+
+    <h2>and</h2>
+
+    <h1>FAHMI</h1>
+
+    <div style="margin-bottom: 4em;" />
+
+    <p>ARE MARRIED</p>
+
+    <div style="margin-bottom: 2em;" />
+
+    <p>ON</p>
+
+    <div style="margin-bottom: 2em;" />
+
+    <h3>11TH APRIL 2025</h3>
+
+    <div style="margin-bottom: 2em;" />
+
+    <p>FRIDAY, AT 8 O'CLOCK IN THE MORNING</p>
+    <p>IN DKI JAKARTA, INDONESIA, PLANET EARTH</p>
+    
+    <div style="margin-bottom: 2em;" />
+
+    <p>AND WE MADE YOU BY PAIR</p>
   </div>
 </template>
+<!-- css -->
+<style scoped>
+p {
+  font-family: 'Roboto Flex', sans-serif;
+  color: #FFEFCF;
+  letter-spacing: 0.5em;
+  margin: 0;
+  padding: 0;
+}
+
+h1,h3 {
+  font-family: 'Cormorant Garamond', serif;
+  color: #FFEFCF;
+  font-size: 4em;
+  letter-spacing: 0.5em;
+  font-weight: 1;
+  margin: 0;
+  padding: 0;
+}
+
+h3 {
+  font-size: 1.8em;
+  letter-spacing: 0.5em;
+  font-weight: 1;
+}
+
+h2 {
+  font-family: 'Lavishly Yours', cursive;
+  color: #FFEFCF;
+  font-size: 3em;
+  margin: 0;
+  padding: 0;
+}
+
+.background {
+  background-image: url('/images/mobile.png');
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+  /* content to be in middle */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,14 +3,28 @@ export default defineNuxtConfig({
   app: {
     baseURL: '/sakinahstory/', // Must match the GitHub repo name
   },
+
   routeRules: {
     '/**': { prerender: true }, // Ensures static generation
   },
+
   nitro: {
     prerender: {
       crawlLinks: true,
     },
   },
+
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      'Cormorant+Garamond': true,
+      'Lavishly+Yours': true,
+      'Roboto+Flex': true
+    },
+    display: 'swap', // Ensures better loading performance
+    prefetch: true,  // Improves loading speed
+    preconnect: true,
+  }
 })
