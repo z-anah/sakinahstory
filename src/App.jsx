a1ff96c6df30858d6e89703b892f20388c0fe2f6
z-anah <40556052+z-anah@users.noreply.github.com>
2025-04-05 18:32:31 +0700

    Revert "_"
    This reverts commit bd91e60d2e61b7aa1d14a3ba1acf87fc911c5821.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
-import Welcome from './screens/Welcome';
 import Home from './screens/Home';
 
 const App = () => {
   const BASE_PATH = import.meta.env.BASE_URL;
 
+  
   return (
-    <Helmet>
-      <meta property="og:title" content="My Page Title" />
-      <meta property="og:description" content="Page description here" />
-      <meta property="og:image" content="https://maxst.icons8.com/vue-static/landings/page-index/upgrade-tools/megaCreator1x.webp" />
-      <meta property="og:url" content="https://yourdomain.com/page-link" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <Routes>
-        <Route path={BASE_PATH} element={<Welcome />} />
-        <Route path={`${BASE_PATH}/home`} element={<Home />} />
-        <Route path="*" element={<Navigate to={BASE_PATH} />} />
-      </Routes>
-    </Helmet>
+    <Routes>
+      <Route path={BASE_PATH} element={<Home />} />
+      <Route path="*" element={<Navigate to={BASE_PATH} />} />
+    </Routes>
   );
 };
 
